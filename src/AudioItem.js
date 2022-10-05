1e9cf3267e27c8e43c536a02a53681b6bb81cf35
Arystan <arystankaliakparov@gmail.com>
2022-10-05 19:11:34 +0600

    toggle all

--- src/AudioItem.js
+++ src/AudioItem.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 class AudioItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.audio = React.createRef()
+  }
+
+  togglePlay = () => {
+    this.props.onTogglePlay(this.props.track)
+  }
+
   render() {
     return (
       <div className="item">
-        <i className="large middle aligned icon play" />
+        <i 
+          className={`large middle aligned icon ${this.props.isPlaying ? 'pause' : 'play'}`}
+          onClick={() => this.togglePlay()}
+        />
         <div className="content">
           <div className="header">{this.props.track.name}</div>
           <div className="meta">
--- src/AudioList.js
+++ src/AudioList.js
@@ -1,15 +1,38 @@
 import React from "react";
 import AudioItem from "./AudioItem";
 
-function AudioList({ tracks }) {
-  const list = tracks.map(track => (
-    <AudioItem key={track.id} track={track} />
-  ))
-  return (
-    <div className="ui relaxed divided list">
-      {list}
-    </div>
-  )
+class AudioList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { playingAudio: null, sound: null }
+  }
+
+  togglePlay = (audio) => {
+    if (this.state.playingAudio) {
+      this.state.sound?.pause()
+    }
+    if (audio.id !== this.state.playingAudio.id) {
+      const sound = new Audio(audio.previewURL)
+      sound.play()
+      this.setState({ sound: sound, playingAudio: audio })
+    }
+  }
+
+  render() {
+    const list = this.props.tracks.map(track => (
+      <AudioItem 
+        key={track.id}
+        track={track}
+        onTogglePlay={this.togglePlay}
+        isPlaying={track.id === this.state.playingAudio?.id}
+      />
+    ))
+    return (
+      <div className="ui relaxed divided list">
+        {list}
+      </div>
+    )
+  }
 }
 
 export default AudioList;
